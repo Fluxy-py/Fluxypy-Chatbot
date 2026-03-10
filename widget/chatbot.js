238527d832f021917e807903eea6d59d235931f9
Ansh Mani Tripathi <anshmanitripathi171203@gmail.com>
2026-03-10 22:10:32 +0530

    fixed error

--- widget/chatbot.js
+++ widget/chatbot.js
@@ -9,7 +9,7 @@
   const API_KEY = scriptTag?.getAttribute('data-api-key');
   const API_BASE =
     scriptTag?.getAttribute('data-api-url') ||
-    'http://localhost:3001/api/v1';
+    'http://localhost:3001/api/v1' || 'https://fluxypy-chat-api.onrender.com';
 
   const WIDGET_VERSION = '1.0.0';
 
